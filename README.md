d90ce979f455588bbb5beb381921c326304a7332
Timo Schotka <timo.schotka@synaigy.com>
2020-12-30 12:22:41 +0100

    Refactoring

--- README.md
+++ README.md
@@ -7,8 +7,18 @@ To open the interactive elements wizard click on the magic wand which apears if 
 
 ![](https://raw.githubusercontent.com/tisch019/Markdown-Interactive/main/docs/assistantButton.png)
 
-You can see the interactive elements in the preview mode. To use is it, you have to enable scripts to the preview.
+You can see the interactive elements in the preview mode. To use them, you have to enable scripts to the preview.
 
 To save the preview in html format, install the plugin "Markdown All in One" and click on "Print current document to HTML".
+
+## Supported interactive elements
+### Gallery
+![](https://raw.githubusercontent.com/tisch019/Markdown-Interactive/main/docs/syntaxGallery.png)
+
+### Multiple choice test
+![](https://raw.githubusercontent.com/tisch019/Markdown-Interactive/main/docs/syntaxMct.png)
+
+### Map
+![](https://raw.githubusercontent.com/tisch019/Markdown-Interactive/main/docs/syntaxMap.png)
 
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 import MarkdownIt = require('markdown-it');
 import markdownItInteractive from './markdownItInteractive';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	const editor = vscode.window.activeTextEditor;
 	// Track currently webview panel
 	let currentPanel: vscode.WebviewPanel | undefined = undefined;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	//console.log('Congratulations, your extension "Markdown interactive" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('markdown.addElement', () => {
-		// The code you place here will be executed every time your command is executed
 		const columnToShowIn = vscode.window.activeTextEditor
         ? vscode.window.activeTextEditor.viewColumn
         : undefined;
